9cfa0c16e9e7e5aa60e86c87b3fc3bcb49b899d2
stolomeo <tolomeosebastian@gmail.com>
2022-02-03 00:27:23 -0500

    Added keyboard support and styles

--- script.js
+++ script.js
@@ -10,15 +10,11 @@ let previousDisplayNum = '';
 let currentDisplayNum = '';
 let lastOperator = '';
 let result = null;
-let hasDecimal = false;
 
 numbers.forEach(number => {
     number.addEventListener('click', (e) => {
-        if (e.target.textContent == '.' && !hasDecimal) {
-            hasDecimal = true;
-        } else if (e.target.textContent == '.' && hasDecimal) {
-            return;
-        }
+        ifNope();
+        if (e.target.textContent == '.' && currentDisplayNum.includes('.')) return;
         currentDisplayNum += e.target.textContent
         currentDisplay.textContent = currentDisplayNum;
     })
@@ -26,8 +22,8 @@ numbers.forEach(number => {
 
 operations.forEach(operator => {
     operator.addEventListener('click', (e) => {
+        ifNope();
         if (!currentDisplayNum) return;
-        hasDecimal = false;
         const operationName = e.target.textContent;
         if (currentDisplayNum && previousDisplayNum && lastOperator) {
             operate(lastOperator, previousDisplayNum, currentDisplayNum);
@@ -41,7 +37,6 @@ operations.forEach(operator => {
 
 equalsBtn.addEventListener('click', (e) => {
     if (!currentDisplayNum || !previousDisplayNum) return;
-    hasDecimal = false;
     operate(lastOperator, previousDisplayNum, currentDisplayNum);
     clearDisplay();
     currentDisplay.textContent = result;
@@ -49,13 +44,62 @@ equalsBtn.addEventListener('click', (e) => {
     previousDisplayNum = '';
 })
 
-clearBtn.addEventListener('click', (e) => {
-    previousDisplay.textContent = '0';
-    currentDisplay.textContent = '0';
-    previousDisplayNum = '';
-    currentDisplayNum = '';
-    result = '';
+clearBtn.addEventListener('click', clearCalc)
+
+deleteBtn.addEventListener('click', (e) => {
+    currentDisplayNum = currentDisplayNum.toString().slice(0,-1);
+    currentDisplay.textContent = currentDisplay.textContent.toString().slice(0,-1);
 })
+
+window.addEventListener('keydown', (e) => {
+    if (e.key == '1' || e.key == '2' || e.key == '3' || e.key == '4' || 
+        e.key == '5' || e.key == '6' || e.key == '7' || e.key == '8' ||
+        e.key == '9' || e.key == '0' || e.key == '.') {
+        numberKeys(e.key);
+    } else if (e.key == '+' || e.key == '-' || e.key == '*' || e.key == '/') {
+        operatorKeys(e.key);
+    } else if (e.key == '=') {
+        equalsKey(e.key)
+    } else if (e.key == 'Backspace') {
+        deleteKey(e.key);
+    } else if (e.key == "Escape") {
+        clearKey(e.key);
+    }
+})
+
+function numberKeys(key) {
+    numbers.forEach(number => {
+        if (number.textContent == key) {
+            number.click();
+        }
+    })
+}
+
+function operatorKeys(key) {
+    operations.forEach(operator => {
+        if (operator.textContent == key) {
+            operator.click();
+        }
+    })
+}
+
+function equalsKey(key) {
+    if (equalsBtn.textContent == key) {
+        equalsBtn.click();
+    }
+}
+
+function deleteKey(key) {
+    if (key == 'Backspace') {
+        deleteBtn.click();
+    }
+}
+
+function clearKey(key) {
+    if (key == 'Escape') {
+        clearBtn.click();
+    }
+}
 
 function operate(lastOperator, a, b) {
     if (lastOperator == '+') {
@@ -66,30 +110,33 @@ function operate(lastOperator, a, b) {
         result = multiply(result, parseFloat(currentDisplayNum));
     } else if (lastOperator == '/') {
         if (currentDisplayNum == 0) {
-            result = 'NOPE'
+            result = 'NOPE';
         } else {
             result = divide(result, parseFloat(currentDisplayNum));
         }
-        
     }
 }
 
-deleteBtn.addEventListener('click', (e) => {
-    currentDisplayNum = currentDisplayNum.toString().slice(0,-1);
-    currentDisplay.textContent = currentDisplay.textContent.toString().slice(0,-1);
-    console.log(currentDisplay);
-})
-
-
 function clearDisplay (opName = '') {
-    previousDisplayNum += currentDisplayNum + ' ' + opName + ' ';
+    previousDisplayNum += (currentDisplayNum) + ' ' + opName + ' ';
     previousDisplay.textContent = previousDisplayNum;
     currentDisplay.textContent = result;
     currentDisplayNum = '';
 }
 
+function clearCalc () {
+    previousDisplay.textContent = '0';
+    currentDisplay.textContent = '0';
+    previousDisplayNum = '';
+    currentDisplayNum = '';
+    result = '';
+}
+
+function ifNope() {
+    if (result == 'NOPE') clearCalc();
+}
+
 function add(a,b) {
-    console.log(currentDisplayNum);
     return a+b;
 }
 function subtract(a,b) {
